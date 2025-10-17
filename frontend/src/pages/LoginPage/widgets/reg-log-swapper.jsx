f0f4d93ec0b87b36b03e0e1a1df80942e1da20be
unwelcome <moyapohtaxd@mail.ru>
2025-10-18 00:50:30 +0300

    fix(login page): fix visual bug on swapper

--- frontend/src/pages/LoginPage/widgets/reg-log-swapper.jsx
+++ frontend/src/pages/LoginPage/widgets/reg-log-swapper.jsx
@@ -3,9 +3,9 @@ import clsx from 'clsx';
 function SwapperWidget({selected, setSelected}){
 
   return (
-    <div className="flex flex-row gap-2 items-stretch py-1 px-2 bg-1 rounded-md">
-      <div className={clsx("w-1/2 shrink-0 px-2 py-0.5 text-center cursor-pointer select-none rounded text-lg", selected && "bg-0")} onClick={() => setSelected(true)}>Вход</div>
-      <div className={clsx("w-1/2 shrink-0 px-2 py-0.5 text-center cursor-pointer select-none rounded text-lg", !selected && "bg-0")} onClick={() => setSelected(false)}>Регистрация</div>
+    <div className="grid grid-cols-2 gap-2 py-1 px-2 bg-1 rounded-md">
+      <div className={clsx("px-2 py-0.5 text-center cursor-pointer select-none rounded text-lg", selected && "bg-0")} onClick={() => setSelected(true)}>Вход</div>
+      <div className={clsx("px-2 py-0.5 text-center cursor-pointer select-none rounded text-lg", !selected && "bg-0")} onClick={() => setSelected(false)}>Регистрация</div>
     </div>
   )
 }
